ad0e87d4d558011b5aebd371b97319adea7a0fc1
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-07 13:46:47 -0800

    Add tests for Demographics class

--- __tests__/demographics.test.js
+++ __tests__/demographics.test.js
@@ -8,4 +8,27 @@ describe("Demographics", () => {
     expect(demographics).toBeInstanceOf(Demographics);
   });
 
+  test("should correctly assign true/false to daily exercise", () => {
+    demographics.assignDailyExercise(true);
+    expect(demographics.exerciseDaily).toEqual(true);
+  });
+
+  test("should correctly assign 'normal' to BMI", () => {
+    demographics.assignBMI("Normal");
+    expect(demographics.bmi).toEqual("Normal");
+  });
+
+  test("should correctly assign a job title to occupation", () => {
+    demographics.assignOccupation("Professional Rock Climber");
+    expect(demographics.occupation).toEqual("Professional Rock Climber");
+  });
+
+  test("key-pair values should correctly matches the expected properties", () => {
+    expect.objectContaining({
+      exerciseDaily: expect.any(Boolean),
+      bmi: expect.any(String),
+      occupation: expect.any(String)
+    });
+  });
+
 });
