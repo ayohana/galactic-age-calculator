638e6914d85be041cd47bf4895a549f831d09cf0
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-07 10:44:19 -0800

    Add first Jest test and update JS file name

---
+++ __tests__/age-calculator.test.js
@@ -0,0 +1,17 @@
+import { User } from "../src/user";
+
+describe("User", () => {
+  
+  let inputAge;
+  let user;
+
+  beforeEach(() => {
+    inputAge = 25;
+    user = new User(inputAge);
+  });
+  
+
+  test("should correctly create a user object with their Earth age", () => {
+    expect(user.earthAge).toEqual(25);
+  });
+});
