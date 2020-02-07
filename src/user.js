fc2af72f3551ae0629bdee05f42d5a98a34cc073
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-07 11:27:30 -0800

    Add calculator methods to User Class

--- src/user.js
+++ src/user.js
@@ -2,9 +2,24 @@ export class User {
   constructor(age) {
     this.earthAge = age;
     this.mercuryAge = this.calculateMercuryAge();
+    this.venusAge = this.calculateVenusAge();
+    this.marsAge = this.calculateMarsAge();
+    this.jupiterAge = this.calculateJupiterAge();
   }
 
   calculateMercuryAge() {
     return this.earthAge * 0.24;
   }
-};
+
+  calculateVenusAge() {
+    return this.earthAge * 0.62;
+  }
+
+  calculateMarsAge() {
+    return this.earthAge * 1.88;
+  }
+
+  calculateJupiterAge() {
+    return this.earthAge * 11.86;
+  }
+}
