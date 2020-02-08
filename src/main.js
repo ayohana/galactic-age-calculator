73ea90be04a628217988b78a81dc75d2eeb28a6b
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-07 17:22:05 -0800

    Add a function to enter new user input values in main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 // import $ from 'jquery';
-// import { User } from '../src/user';
+import { User } from '../src/user';
+
+function newUser(age) {
+  let user = new User(age);
+  user.demographics.assignDailyExercise(false);
+  user.demographics.assignBMI("Slightly Obese");
+  user.demographics.assignOccupation("Couch Potato");
+  user.checkLife();
+  // Insert console.log(user) here to check object key-pair values
+}
+
+const inputAge = 50;
+newUser(inputAge);
+
+
+
