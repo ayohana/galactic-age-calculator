417669cec24ac6865362234084b489dc9e03c615
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-07 16:31:42 -0800

    Update methods to calculate extra life

--- src/user.js
+++ src/user.js
@@ -3,46 +3,69 @@ import { Demographics } from "../src/demographics";
 export class User {
   constructor(age) {
     this.earthAge = age;
-    this.mercuryAge = this.calculateMercuryAge();
-    this.venusAge = this.calculateVenusAge();
-    this.marsAge = this.calculateMarsAge();
-    this.jupiterAge = this.calculateJupiterAge();
-    this.saturnAge = this.calculateSaturnAge();
-    this.uranusAge = this.calculateUranusAge();
-    this.neptuneAge = this.calculateNeptuneAge();
+    this.mercuryAge = this.calculateMercuryAge(age);
+    this.venusAge = this.calculateVenusAge(age);
+    this.marsAge = this.calculateMarsAge(age);
+    this.jupiterAge = this.calculateJupiterAge(age);
+    this.saturnAge = this.calculateSaturnAge(age);
+    this.uranusAge = this.calculateUranusAge(age);
+    this.neptuneAge = this.calculateNeptuneAge(age);
     this.demographics = new Demographics();
   }
 
-  calculateMercuryAge() {
-    return this.earthAge * 0.24;
+  calculateMercuryAge(earthAge) {
+    return earthAge * 0.24;
   }
 
-  calculateVenusAge() {
-    return this.earthAge * 0.62;
+  calculateVenusAge(earthAge) {
+    return earthAge * 0.62;
   }
 
-  calculateMarsAge() {
-    return this.earthAge * 1.88;
+  calculateMarsAge(earthAge) {
+    return earthAge * 1.88;
   }
 
-  calculateJupiterAge() {
-    return this.earthAge * 11.86;
+  calculateJupiterAge(earthAge) {
+    return earthAge * 11.86;
   }
 
-  calculateSaturnAge() {
-    return this.earthAge * 0.08;
+  calculateSaturnAge(earthAge) {
+    return earthAge * 0.08;
   }
 
-  calculateUranusAge() {
-    return this.earthAge * 0.23;
+  calculateUranusAge(earthAge) {
+    return earthAge * 0.23;
   }
 
-  calculateNeptuneAge() {
-    return this.earthAge * 0.45;
+  calculateNeptuneAge(earthAge) {
+    return earthAge * 0.45;
   }
 
   calculateRemainingLife() {
-    return this.remainingLife = Math.abs(this.demographics.lifeExpectancy - this.earthAge);
+    let lifeExpectancyOnEarth = this.demographics.lifeExpectancy;
+    this.remainingLifeOnEarth = (lifeExpectancyOnEarth - this.earthAge).toFixed(1);
+    this.remainingLifeOnMercury = (this.calculateMercuryAge(lifeExpectancyOnEarth) - this.mercuryAge).toFixed(1);
+    this.remainingLifeOnVenus = (this.calculateVenusAge(lifeExpectancyOnEarth) - this.venusAge).toFixed(1);
+    this.remainingLifeOnMars = (this.calculateMarsAge(lifeExpectancyOnEarth) - this.marsAge).toFixed(1);
+    this.remainingLifeOnJupiter = (this.calculateJupiterAge(lifeExpectancyOnEarth) - this.jupiterAge).toFixed(1);
+    this.remainingLifeOnSaturn = (this.calculateSaturnAge(lifeExpectancyOnEarth) - this.saturnAge).toFixed(1);
+    this.remainingLifeOnUranus = (this.calculateUranusAge(lifeExpectancyOnEarth) - this.uranusAge).toFixed(1);
+    this.remainingLifeOnNeptune = (this.calculateNeptuneAge(lifeExpectancyOnEarth) - this.neptuneAge).toFixed(1);
+  }
+
+  calculateExtraLife() {
+    if (Math.sign(this.remainingLifeOnEarth) === -1) {
+      this.extraLifeOnEarth = Math.abs(this.remainingLifeOnEarth);
+      this.extraLifeOnMercury = Math.abs(this.remainingLifeOnMercury);
+      this.extraLifeOnVenus = Math.abs(this.remainingLifeOnVenus);
+      this.extraLifeOnMars = Math.abs(this.remainingLifeOnMars);
+      this.extraLifeOnJupiter = Math.abs(this.remainingLifeOnJupiter);
+      this.extraLifeOnSaturn = Math.abs(this.remainingLifeOnSaturn);
+      this.extraLifeOnUranus = Math.abs(this.remainingLifeOnUranus);
+      this.extraLifeOnNeptune = Math.abs(this.remainingLifeOnNeptune);
+    } else {
+      this.extraLifeOnAnyPlanet = 0;
+    }
   }
 
 }
